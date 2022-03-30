0617adbf5703f80dd36855cad408deeda118321b
diiiiana99 <93669061+diiiiana99@users.noreply.github.com>
2022-03-30 14:13:14 -0400

    Finished Login

--- db/migrate/20220329200239_create_users.rb
+++ db/migrate/20220329200239_create_users.rb
@@ -3,7 +3,8 @@ class CreateUsers < ActiveRecord::Migration[6.1]
     create_table :users do |t|
       t.string :password_digest
       t.string :username
-      
+      t.string: :email
+
       t.timestamps
     end
   end
