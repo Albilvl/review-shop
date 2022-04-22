c63a74e2074acf11e34cf90b525fa33307639295
Al Bilal <bil.bank@yahoo.com>
2022-04-21 22:19:55 -0400

    done

--- client/components/Home/Searchbar.jsx
+++ client/components/Home/Searchbar.jsx
@@ -1,4 +1,4 @@
-import { View, Text,  } from 'react-native'
+import { View, Text,  } from 'react-native'g
 import React from 'react'
 import {GooglePlacesAutocomplete} from 'react-native-google-places-autocomplete'
 import{ Ionicons, AntDesign} from 'react-native-vector-icons'
